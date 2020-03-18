1d48bd0547956d4677ef6552baebc921e324dba8
juliaokataleko <juliofeli78@gmail.com>
2020-03-18 17:45:00 +0000

    actualizando o ficheiro product.controller.ts

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 
 import { CreateProductDTO } from './dto/product.dto';
 
@@ -35,5 +35,25 @@ export class ProductController {
         const product = await this.productService.getProduct(productID);
         if(!product) throw new NotFoundException('Este Produto Nao Existe');
         return res.status(HttpStatus.OK).json(product)
+    }
+
+    @Delete('/delete')
+    async deleteProduct(@Res() res, @Query('productID') productID) {
+        const productDeleted = await this.productService.deleteProduct(productID);
+        if(!productDeleted) throw new NotFoundException('Este Produto Nao Existe');
+        return res.status(HttpStatus.OK).json({
+            message: 'Produto Excluido Com sucesso',
+            productDeleted
+        })
+    }
+
+    @Put('/update')
+    async updateProduct(@Res() res, @Body() createProductDTO: CreateProductDTO, @Query('productID') productID) {
+        const updatedProduct = await this.productService.updateProduct(productID, createProductDTO);
+        if(!updatedProduct) throw new NotFoundException('Este Produto Nao Existe');
+        return res.status(HttpStatus.OK).json({
+            message: 'Produto actualizado Com sucesso',
+            updatedProduct
+        })
     }
 }
